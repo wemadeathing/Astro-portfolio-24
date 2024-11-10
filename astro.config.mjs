9f8f85c916d0685f7d1bc59ee73666a620e5e861
wemadeathing <salaam.nasif@gmail.com>
2024-11-10 14:56:03 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   output: 'hybrid',
-
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -15,6 +14,4 @@ export default defineConfig({
     react(),
     mdx(),
   ],
-
-  adapter: netlify(),
 });
