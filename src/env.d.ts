ef1cbf5e96e62f215fe82c182452a4164a35b482
wemadeathing <salaam.nasif@gmail.com>
2024-11-11 21:43:32 +0200

    blogand portfolio refinements

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
 interface ImportMetaEnv {
   readonly RESEND_API_KEY: string;
   readonly CONTACT_EMAIL: string;
