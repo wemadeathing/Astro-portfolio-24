627c318c64f6175a207fbd4acf397c8a1860921e
wemadeathing <salaam.nasif@gmail.com>
2025-09-15 09:06:07 +0200

    Refactor build files and update middleware secret; remove obsolete chunks and update imports in entry and renderers files.

--- .netlify/build/chunks/journey-from-design-to-development-ai-shift_2BqR0XNb.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./journey-from-design-to-development-ai-shift_Bt6NCAvi.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
